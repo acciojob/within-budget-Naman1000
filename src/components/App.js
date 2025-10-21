41c7cbbdbff3bc9ac9e384697d61c88abdb37a8a
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-10-22 02:07:59 +0530

    Refactor App component to functional syntax2
    Refactor App component to use functional component syntax and manage price state.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,45 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+export default function App() {
+  
+  const [price ,setPrice ] = useState(0);
+
+  const items = [
+    { name: "Groceries", value: 80 },
+    { name: "Shoes", value: 120 },
+    { name: "Book", value: 50 },
+    { name: "Watch", value: 150 },
+  ];
+
+  return (
+    <div id="container">
+      <h1>Within Budget</h1>
+      <input type = "number"
+      value = {price} 
+      onChange = {(e) => setPrice(e.target.value)}/>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => (
+            <tr
+              key={index}
+              style={{
+                color: item.value > price ? "red" : "green",
+              }}
+            >
+              <td>{item.name}</td>
+              <td>{item.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
